38529757e3cb8e093b339e55ac1c935473f75d58
Nikin Baidar <nikinbaidarr@gmail.com>
2022-04-04 10:02:03 +0545

    optimized the replacement function. need to add docstring for the transformImage function

--- python/ImageProcessing/histogram_equalization.py
+++ python/ImageProcessing/histogram_equalization.py
@@ -1,19 +1,23 @@
-#  _____________________________ 
-# / Point Operations: Histogram \
-# \ Equalization                /
-#  ----------------------------- 
-#         \   ^__^
-#          \  (oo)\_______
-#             (__)\       )\/\
-#                 ||----w |
-#                 ||     ||
+#!/usr/bin/env python
+#  __________________________________________________________
+# /\                                                         \
+# \_| Filename    : histogram_equalization.py                |
+#   | Maintainer  : Nikin Baidar                             |
+#   | Description : Point Operation (Histogram Equlaization) |
+#   |   _____________________________________________________|_
+#    \_/_______________________________________________________/
+
+
+from matplotlib import pyplot
 
 import cv2
 import numpy
-from matplotlib import pyplot
+
 
 def equalilzeHistogram(histogram, resolution):
-  """ ## Applies CDF to a histogram and returns the equalized histogram.
+  """ ## Applies CDF to a histogram and returns the equalized histogram. The 
+  size of the equlaized histogram may not be same as the original depending on
+  how many unique pixels exists in the image.
   ### (CDF) Cumulative Distribution function:
       $$
       CDF(i) = \sum_{j<=i} h(j)
@@ -26,86 +30,92 @@ def equalilzeHistogram(histogram, resolution):
         - L is the highest possible pixel intensity.
   ## Parameters:
      * histogram: The input histogram that is to be equlaized
-     * resolution: resolution of the original image. """
+     * resolution: resolution of the original image.
+  ## Return Type: <class : 'list'>. """
 
-  def computeCDF():
-    # List the frequecny of those unique intensity values
-    grey_level_frequency = numpy.array([ histogram[value] 
-      for value in range(256) if histogram[value] ]).reshape(-1,1)
+  def computeCDF(histogram):
+    # List the frequecies of all unique intensity values
+    # Image histogram but only existing grey levels are listed
+    unique_grey_level_frequencies = numpy.array([histogram[value] 
+      for value in range(256) if histogram[value]])
+    unique_gray_level_count = unique_grey_level_frequencies.shape[0]
 
-    unique_gray_level_count = numpy.shape(grey_level_frequency)[0]
-
-    cdf = numpy.array([ numpy.sum(grey_level_frequency[0:index+1]) 
-      for index in range(unique_gray_level_count) ]).reshape(-1,1)
+    cdf = numpy.array([numpy.sum(unique_grey_level_frequencies[0:index+1]) 
+      for index in range(unique_gray_level_count)])
 
     return cdf
 
-  def normalizeCDF():
-    """ Returns the equalized histogram. """
-    hist_equalized = numpy.array(numpy.zeros(cdf.shape[0])).reshape(-1,1)
-    scaling_factor = 256 - 1
-    cdf_minimum    = numpy.min(cdf)
-    denominator    = resolution - cdf_minimum
-
-    for item in range(cdf.shape[0]):
-      numerator = cdf[item] - cdf_minimum
-      hist_equalized[item] = numerator / denominator * scaling_factor
-
-    return numpy.round(hist_equalized)
-
-  # List all unique pixel intensity values in the image
-  existing_pixels = numpy.array([ value for value in range(256) 
-     if histogram[value] ]).reshape(-1,1)
-
   # Acquire the CDF values
-  cdf = computeCDF()
-  histogram_equalized = normalizeCDF()
-  table = numpy.hstack((existing_pixels, histogram_equalized))
+  cdf = computeCDF(histogram)
 
-  return table
+  hist_equalized = numpy.array(numpy.zeros(cdf.shape[0])).reshape(-1,1)
 
-def main():
+  # Apply the transformation function
+  scaling_factor = 256 - 1
+  cdf_minimum    = numpy.min(cdf)
+  denominator    = resolution - cdf_minimum
 
-  image = cv2.imread("./images/Lenna.png", 0)
+  for item in range(cdf.shape[0]):
+    numerator = cdf[item] - cdf_minimum
+    hist_equalized[item] = numerator / denominator * scaling_factor
 
-  cv2.imshow("Original", image)
-  cv2.waitKey()
+  hist_equalized =  numpy.round(hist_equalized)
 
-  # image = numpy.array([[52, 55, 61,  59,  70,  61, 76, 61],
-  #                      [62, 59, 55, 104,  94,  85, 59, 71],
-  #                      [63, 65, 66, 113, 144, 104, 63, 72],
-  #                      [64, 70, 70, 126, 154, 109, 71, 69],
-  #                      [67, 73, 68, 106, 122,  88, 68, 68],
-  #                      [68, 79, 60,  79,  77,  66, 58, 75],
-  #                      [69, 85, 64,  58,  55,  61, 65, 83],
-  #                      [70, 87, 69,  68,  65,  73, 78, 90]], 
-  #                      dtype=numpy.uint8)
+  return list(hist_equalized)
+
+
+def transformHistogram(image):
 
   rows        = image.shape[0]
   columns     = image.shape[1]
   resolution  = rows * columns
 
-  # Compute the image histogram!
   image_histogram = cv2.calcHist([image], [0], None, [256], [0,255])
+  hist_equalized  = equalilzeHistogram(image_histogram, resolution)
 
-  table = equalilzeHistogram(image_histogram, resolution)
+  # List all unique pixel intensity values in the image
+  existing_pixels = [value for value in range(256) if image_histogram[value]]
 
-  print(table)
-
-  # x = numpy.where(table==68)
-  # print(x)
-
-  # Replace Pixel values with the normalized CDF Values
+  # Replace existing pixels with the new (equalized) histogram values
   for i in range(rows):
     for j in range(columns):
-      # Comput the row of the table that contains replacement for image[i,j]
-      x = numpy.where(table==image[i][j])[0][0] 
-      image[i,j] = table[x][1]
+      index       = existing_pixels.index(image[i,j])
+      replacement = hist_equalized[index]
+      image[i,j]  = replacement
 
   image = numpy.array(image, dtype=numpy.uint8)
+
+  return image
+
+
+def main():
+
+  # image = cv2.imread("./images/Lenna.png", 0)
+
+  # # Display the input image
+  # cv2.imshow("Original", image)
+  # cv2.waitKey()
+
+  image = numpy.array([[52, 55, 61,  59,  70,  61, 76, 61],
+                       [62, 59, 55, 104,  94,  85, 59, 71],
+                       [63, 65, 66, 113, 144, 104, 63, 72],
+                       [64, 70, 70, 126, 154, 109, 71, 69],
+                       [67, 73, 68, 106, 122,  88, 68, 68],
+                       [68, 79, 60,  79,  77,  66, 58, 75],
+                       [69, 85, 64,  58,  55,  61, 65, 83],
+                       [70, 87, 69,  68,  65,  73, 78, 90]], 
+                       dtype=numpy.uint8)
+  
   print(image)
-  cv2.imshow("Transformed", image)
-  cv2.waitKey()
+
+  transfomred_image = transformHistogram(image)
+
+  print()
+  print(transfomred_image)
+
+  # # Display Transformed Image
+  # cv2.imshow("Transformed", transfomred_image)
+  # cv2.waitKey()
  
 if __name__ == '__main__':
   main()
