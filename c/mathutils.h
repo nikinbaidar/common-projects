583e112c93314bc71365f4c6245b3f7520522080
Nikin Baidar <nikinbaidarr@gmail.com>
2023-03-22 08:48:53 +0545

    simple math relations -> mathutils.h

--- c/mathutils.h
+++ c/mathutils.h
@@ -6,40 +6,28 @@ static bool inRange(int a, int lower, int upper);
 static float getDecimalPart(float num);
 static double power(double x, double n);
 
-bool 
-isFactor(int a, int b) 
-{
+bool isFactor(int a, int b) {
   return ((a/b) * b == a) ? 1 : 0;
 }
 
 
-bool
-isEven(int a) 
-{
+bool isEven(int a) {
   return !(a & 1);
 }
 
 
-bool 
-inRange(int a, int lower, int upper) 
-{
+bool inRange(int a, int lower, int upper) {
   return (a >= lower && a <= upper);
 }
 
 
-float
-getDecimalPart(float num) 
-{
+float getDecimalPart(float num) {
     return (num - (int) num);
 }
 
 
-double
-power(double x, double n)
-{
+double power(double x, double n) {
     double result;
     result = (floor(n/2) > 0) ? x * power(x, floor(n/2) - 1) : 1;
     return isEven(n) ? result * result : x * result * result;
 }
-
-
