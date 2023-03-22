944bfe08a7096c2db8005a1c07d0067a34f3c1dc
Nikin Baidar <nikinbaidarr@gmail.com>
2023-03-22 08:44:22 +0545

    recurse array to insertion sort

--- c/insertionsort.c
+++ c/insertionsort.c
@@ -1,38 +1,9 @@
 #include <stdio.h>
-#include "array_related_functions.h"
+#include "array.h"
 
-static int insertionSort(int x[], int n);
-static int insert(int x[], int i, int a);
-
-
-int insertionSort (int x[], int n) {
-  if (n > 0) {
-    insertionSort(x, n-1);
-    insert(x, n, x[n]);
-  }
-  return 0;
-}
-
-int insert (int x[], int i, int a) {
-  if (i == 0 || x[i-1] <= a)
-    x[i] = a;
-  else {
-    x[i] = x[i-1];
-    insert(x, i-1, a);
-  }
-  return 0; 
-}
-
-
-int main() {
-  int x[] = {15, 8, 6, 3, 2, 37, 24};
-  unsigned int length_x = getSize(x);
-
-  printf("Before sorting: ");
-  printArr(x, length_x);
-
-  insertionSort(x, length_x);
-
-  printf("\nAfter  sorting: ");
-  printArr(x, length_x);
+void main() {
+    int myArray [] = {2, 6, 4, 0, 5, 1, 3};
+    const int end = getEndIndex(myArray);
+    insertionSort(myArray, end);
+    printArray(myArray, end);
 }
--- c/testing.c
+++ c/testing.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 #include "simple_math_relations.h"
+#include "array_related_functions.h"
 
-int main () {
-  int num = 102 , lower = 10 , upper = 20;
-
-  (inRange(num, lower, upper)) ? printf("Yes") : printf("No");
-
-  return 0;
+int main () 
+{
+    float p = getDecimalPart(9.81);
+    printf("%f\n", p);
+    
+    return 0;
 }
+
+
