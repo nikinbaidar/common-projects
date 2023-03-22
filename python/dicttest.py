c5f1a8e46a940fec572393d651e1d6525686e466
Nikin Baidar <nikinbaidarr@gmail.com>
2023-03-22 08:56:13 +0545

    testing py tables

--- python/dicttest.py
+++ python/dicttest.py
@@ -1,28 +1,10 @@
-#! /usr/bin/python 
+#!/usr/bin/python 
 
-import numpy as np
+tumours = {
+        0 : 'glioma_tumour',
+        1 : 'pitutary_tumor',
+        3 : 'meningioma_tumor',
+        4 : 'no_tumor'
+        }
 
-labels =  {
-    'glioma_tumor'    : 0,
-    'meningioma_tumor': 1,
-    'no_tumor'        : 2,
-    'pituitary_tumor' : 3
-}
-
-# for label in labels.keys():
-#     print(label)
-
-# y = [item for item in range(len(labels.keys()))]
-
-# print(y)
-
-# x = list(np.ones_like(y, dtype=np.int8) * 2)
-
-# print(x)
-
-urls =  {
-    ['a', 'b'] : 0,
-    ['c', 'd'] : 1,
-}
-
-print(urls)
+print(tumours)
